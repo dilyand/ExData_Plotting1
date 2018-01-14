132d8b78035bfcb58927f95402b62bd05608078a
Dilyan Damyanov <dilyand@gmail.com>
2018-01-14 16:17:30 +0000

    Comment out download section in plot3.R

--- plot3.R
+++ plot3.R
@@ -20,13 +20,13 @@
 ### Download and unzip the file. This works on a Windows machine. You might need
 ### to use the appropriate method on another OS.
 
- fileUrl <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+# fileUrl <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
 
 # Download the zip file in the current working directory.
- download.file(fileUrl, destfile = "./power_consumption.zip")
+# download.file(fileUrl, destfile = "./power_consumption.zip")
 
 # Unzip the file in the current working directory.
- unzip("./power_consumption.zip", exdir = getwd())
+# unzip("./power_consumption.zip", exdir = getwd())
 
 ### Read in the dataset.
 
